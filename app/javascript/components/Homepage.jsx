b28688b3f964bfe926e390082f4173d8d7f40017
Isadora Barros <isa.dora786@gmail.com>
2020-03-28 20:10:18 -0300

    Fix homepage styling

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -5,13 +5,13 @@ import { Box } from '@material-ui/core';
 
 export default props =>
   <Fragment>
-      <AppMenu/>
-      <Typography variant='h2' align='center'>
-        <Box fontWeight='fontWeightBold'>
-          TODO List
-        </Box>
-      </Typography>
-      <Typography variant='h4' align='center'>
-        Your daily helper on task managing!
-      </Typography>
+    <AppMenu/>
+    <Typography variant='h4' align='center' style={{marginTop: '20%'}}>
+      <Box fontWeight='fontWeightBold'>
+        TODO List
+      </Box>
+    </Typography>
+    <Typography variant='h5' align='center'>
+      Your daily helper on task managing!
+    </Typography>
   </Fragment>
