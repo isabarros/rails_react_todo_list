d325300c0e93de51a9b870a9200a571c93cb8d44
Isadora Barros <isa.dora786@gmail.com>
2020-06-24 22:41:36 -0300

    Add users to db seeds file

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ To Do List app for created using React and Ruby on Rails
 1. You need to have your application up and running (`docker-compose up`)
 1. In another terminal execute `docker-compose run web bundle exec rake db:migrate`
 
+## How to populate db with seed data
+Run `docker-compose run web bundle exec rake db:seed`
+
 ## Troubleshooting Docker on Linux
 (note: in case permission denied while trying to connect to the Daemon `sudo chmod 666 /var/run/docker.sock`)
 (note2: https://docs.docker.com/engine/install/linux-postinstall/#configure-docker-to-start-on-boot)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'amit', email: 'amit.choudhary@bigbinary.com', password: 'amit.choudhary')
+User.create(username: 'nadine', email: 'nadine@bigbinary.com', password: 'asd678asd')
+User.create(username: 'guga', email: 'guga@bigbinary.com', password: 'shasjdhasjkdsa8789')
+User.create(username: 'luan', email: 'luan@bigbinary.com', password: 'as7d6a8sd6as6d87sad8ads687')
+User.create(username: 'thiago', email: 'thiago@bigbinary.com', password: 'SHSHSJH7886555jjhgy')
